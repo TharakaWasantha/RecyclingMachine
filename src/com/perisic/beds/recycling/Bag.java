24559e6d3ad39f7e2f3217c880f6e2c053179f2c
devilyouknow <salithak1@gmail.com>
2019-10-19 18:59:50 +0530

    updating

---
+++ src/com/perisic/beds/recycling/Bag.java
@@ -0,0 +1,15 @@
+package com.perisic.beds.recycling;
+
+/**
+* Represents a bag to be recycled.
+ * @author Salitha
+ *
+ */
+public class Bag extends DepositItem {
+	/**
+	 * 
+	 */
+	public Bag() { 
+		value = 10; 
+	}
+}
